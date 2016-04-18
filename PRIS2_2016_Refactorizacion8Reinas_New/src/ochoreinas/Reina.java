7dddb1f07f06595fe6aa3c8e72e438d1b373edc2
funo <sabaku.lnx@gmail.com>
2016-04-18 18:46:26 +0200

    Extraer método dibujarCorona(Graphics g) de paint(Graphics g) en la clase Reina

--- PRIS2_2016_Refactorizacion8Reinas_New/src/ochoreinas/Reina.java
+++ PRIS2_2016_Refactorizacion8Reinas_New/src/ochoreinas/Reina.java
@@ -60,8 +60,14 @@ public class Reina {
 			vecina.paint(g);
 		// despues a ella misna
 		// x, y is upper left corner
+		dibujarCorona(g);
+	}
+	
+	public void dibujarCorona(Graphics g){
+		
 		int x = (fila - 1) * 50 + 10;
 		int y = (columna - 1) * 50 + 40;
+		
 		g.drawLine(x + 5, y + 45, x + 45, y + 45);
 		g.drawLine(x + 5, y + 45, x + 5, y + 5);
 		g.drawLine(x + 45, y + 45, x + 45, y + 5);
